bac27fc8d37090923b3b0e8dc4d0c103f1fbcb11
Kyle Timins <rzitex@gmail.com>
2014-03-23 15:13:05 -0400

    Files

--- Project/Website/documentation.php
+++ Project/Website/documentation.php
@@ -1,13 +1,15 @@
 <?php include 'header.php'; ?>
 
+<p>
 The following is the various documentation written for the creation of DRINC in PDF formats.
+</p>
 
-<a href="requirements.pdf">Requirements Documents PLACEHOLDER</a><br />
-<a href="systemsandsoftwarearchitecture.pdf">Systems and Software Architecture PLACEHOLDER</a><br />
-<a href="designdocument.pdf">Design Document PLACEHOLDER</a><br />
-<a href="servertest.pdf">Server Testing Documentation PLACEHOLDER</a><br />
-<a href="interfacetest.pdf">Interface Testing Documentation PLACEHOLDER</a><br />
-<a href="hardwaretest.pdf">Hardware Testing Documentation PLACEHOLDER</a><br />
-<a href="presentation.ppt">Mid-Year Presentation PLACEHOLDER</a><br />
+<a href="Docs/requirements.pdf">Requirements Documents PLACEHOLDER</a><br />
+<a href="Docs/systemsandsoftwarearchitecture.pdf">Systems and Software Architecture PLACEHOLDER</a><br />
+<a href="Docs/design.pdf">Design Document PLACEHOLDER</a><br />
+<a href="Docs/servertest.pdf">Server Testing Documentation PLACEHOLDER</a><br />
+<a href="Docs/interfacetest.pdf">Interface Testing Documentation PLACEHOLDER</a><br />
+<a href="Docs/hardwaretest.pdf">Hardware Testing Documentation PLACEHOLDER</a><br />
+<a href="Docs/presentation.pdf">Mid-Year Presentation PLACEHOLDER</a><br />
 
 <?php include 'footer.php'; ?>
